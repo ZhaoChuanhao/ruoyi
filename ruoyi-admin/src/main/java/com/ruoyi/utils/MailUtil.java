6b49cfc404768ec82ff6603c7576049dcec09db7
赵传昊 <724356121@qq.com>
2019-05-18 21:29:27 +0800

    修改注释

--- ruoyi-admin/src/main/java/com/ruoyi/utils/MailUtil.java
+++ ruoyi-admin/src/main/java/com/ruoyi/utils/MailUtil.java
@@ -29,20 +29,15 @@ public class MailUtil {
         prop.setProperty("mail.host", "smtp.qq.com");
         prop.setProperty("mail.transport.protocol", "smtp");
         prop.setProperty("mail.smtp.auth", "true");
-        // 使用JavaMail发送邮件的5个步骤
-        // 1、创建Session
+        // 创建Session
         Session session = Session.getInstance(prop);
-        // 开启Session的debug模式，这样就可以查看到程序发送Email的运行状态
         session.setDebug(true);
-        // 2、通过session得到transport对象
+        // 通过session得到transport对象
         Transport ts = session.getTransport();
-        // 3、使用邮箱的用户名和密码连上邮件服务器
+        // 使用发件人的用户名和授权码登录到邮件服务器
         ts.connect("smtp.qq.com", Mail.sysSend, Mail.sysPassword);
-        // 4、创建邮件对象
         Message message = createTextMail(session, mail);
-        // 5、发送邮件
         ts.sendMessage(message, message.getAllRecipients());
-
         ts.close();
     }
 
@@ -54,17 +49,11 @@ public class MailUtil {
      * @throws Exception
      */
     public static MimeMessage createTextMail(Session session, Mail mail) throws Exception{
-        // 创建邮件对象
         MimeMessage message = new MimeMessage(session);
-        // 指明邮件的发件人
         message.setFrom(new InternetAddress(mail.getSend()));
-        // 指明邮件的收件人
         message.setRecipient(Message.RecipientType.TO, new InternetAddress(mail.getReceive()));
-        // 邮件的表题
         message.setSubject(mail.getTheme());
-        // 邮件的文本内容
         message.setContent(mail.getContent(), "text/html;charset=UTF-8");
-        // 返回创建好的邮件对象
         return message;
     }
 }
